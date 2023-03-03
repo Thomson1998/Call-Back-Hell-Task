e79242393993e011a1529d85247f563f6f27a98e
Thomson1998 <jackzthomson2@gmail.com>
2023-03-03 21:52:50 +0530

    firstcommit

---
+++ script.js
@@ -0,0 +1,36 @@
+var display = document.querySelector(".display")
+
+var sub = 10
+
+display.innerHTML=sub--
+  setTimeout(()=>{
+    display.innerHTML=sub--
+      setTimeout(()=>{
+        display.innerHTML=sub--
+          setTimeout(()=>{
+            display.innerHTML=sub--
+              setTimeout(()=>{
+                display.innerHTML=sub--
+                  setTimeout(()=>{
+                    display.innerHTML=sub--
+                      setTimeout(()=>{
+                        display.innerHTML=sub--
+                          setTimeout(()=>{
+                            display.innerHTML=sub--
+                              setTimeout(()=>{
+                                display.innerHTML=sub--
+                                  setTimeout(()=>{
+                                    display.innerHTML=sub--
+                                      setTimeout(()=>{
+                                        display.innerHTML=sub--
+                                        display.innerHTML="🎈 Happy Independence Day 🎈"
+                                    },1000)
+                                },1000) 
+                            },1000)
+                        },1000) 
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000)
